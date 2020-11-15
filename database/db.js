4bc3f3773e8bd336ba663a59c44f821f9741f633
Milo Silva <milo.silva234@gmail.com>
2020-11-15 17:07:16 +0100

    refactor sequelize config

--- database/db.js
+++ database/db.js
@@ -1,7 +1,10 @@
 // import Sequelize
 const { Sequelize, Model, DataTypes } = require('sequelize')
-const sequelize = new Sequelize('postgres://millsy@localhost:5432/profile-db')
-
+const sequelize = new Sequelize('postgres://millsy@localhost:5432/')
+const sequelize = new Sequelize('profile-db', 'millsy', null, {
+        // gimme postgres, please!
+        dialect: 'postgres'
+      })
 
 // initialise postgres model ContactForm:
 class ContactForm extends Model {}
