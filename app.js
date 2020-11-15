2d4884973839b54d209e261b6ea1f5bfe70f4747
Milo Silva <milo.silva234@gmail.com>
2020-11-15 17:41:51 +0100

    'server.js' => 'app.js' & httpPort 666 => 80

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const app = express()
 const chalk = require('chalk')
-require('dotenv').config()
-cors = require('cors')
-app.use(cors())
-port = 666
-// import routes
+
+// http(s) modules & https options (TLS certificates) to authenticate https connection
+const http = require('http')
+const https = require('https')
 const httpsOptions = {
     
 }
+
+// env
+require('dotenv').config()
+
+// cors 
+cors = require('cors')
+app.use(cors())
+
+// ports
+const httpPort = 80
+const httpsPort = 443
+
+// import routes
 const ContactsRouter = require('./routes/Contacts')
 
 
@@ -23,10 +35,13 @@ app.route('/')
     res.send(`🎉you've reached the root directory!🎉 here - take some virtual 🍰 I deployed it myself.`)
 })
 app.route(/.*/).all((req,res) => {
-    res.status(404).send('there is no endpoint at this directory')
+    res.status(404).send('No endpoint at this directory')
 })
 // will get this data from front end ContactForms
 
-app.listen(port,() => console.log(`server is live on: \n  ${chalk.blue(`http://localhost:${port}`)} &\n ${chalk.blue(`http://192.168.1.2:${port}`)} `))
+http.createServer(app).listen( httpPort, () => console.log(`httpServer is live on: \n  ${chalk.blue(`http://http://172.105.93.205:${httpPort}`)} `))
+
+// https.createServer(app).listen( httpsPort, () => console.log(`httpsServer is live on: \n  ${chalk.blue(`https://http://172.105.93.205:${httpsPort}`)} `))
+
 
 // sequelize.close() - executed automatically when process terminates. (only needed to manually execute when creating multiple instances and need garbage collection)
